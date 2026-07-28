c8bb4f0d069acb614611a14a59b4b1d421b84173
coderiaser <coderaiser@cloudcmd.io>
2026-07-28 22:40:41 +0300

    chore: lint: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,18 @@
 import {safeAlign} from 'eslint-plugin-putout';
-import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import {defineConfig, globalIgnores} from 'eslint/config';
 
-export default defineConfig(safeAlign, [
+export default defineConfig([
     globalIgnores(['dist']), {
         files: ['**/*.{ts,tsx}'],
         extends: [
-            js.configs.recommended,
             tseslint.configs.recommended,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
+            safeAlign,
         ],
         languageOptions: {
             globals: globals.browser,
