4d911c74d758bc8236c5b75b21a1b9e4ae86e395
coderiaser <coderaiser@cloudcmd.io>
2026-07-28 20:39:21 +0300

    feature: main: add MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import './index.css';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import {
+    ThemeProvider,
+    createTheme,
+    CssBaseline,
+} from '@mui/material';
 import App from './App.tsx';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <App/>
+        </ThemeProvider>
     </StrictMode>,
 );
